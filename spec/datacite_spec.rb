d2a85b76effd77fb12fd94c5b86086d4acfd0725
Justin Coyne <jcoyne@justincoyne.com>
2021-07-06 10:22:36 -0500

    Remove useless test

--- spec/datacite_spec.rb
+++ spec/datacite_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Datacite do
   it "has a version number" do
     expect(Datacite::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
