2a8a3235c54743da60382b04b90e18bded3906b5
Aaron Collier <aaron.collier@stanford.edu>
2025-10-22 15:57:09 -0700

    Move mapping code from cocina-models draft pr

---
+++ lib/datacite/mapping/rights_list.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Datacite
+  module Mapping
+    # Transform the Cocina::Models::DROAccess attributes to the DataCite rightsList attributes
+    #  see https://support.datacite.org/reference/dois-2#put_dois-id
+    class RightsList
+      # @param [Cocina::Models::DROAccess] cocina_item_access
+      # @return [NilClass,Array<Hash>] list of DataCite rightsList attributes, conforming to the expectations of
+      # HTTP PUT request to DataCite
+      def self.build(...)
+        new(...).call
+      end
+
+      def initialize(access:)
+        @access = access
+      end
+
+      # @return [NilClass,Array<Hash>] list of DataCite rightsList attributes, conforming to the expectations of
+      # HTTP PUT request to DataCite
+      def call
+        return if access&.license.blank?
+
+        [{
+          rightsUri: access&.license
+        }]
+      end
+
+      private
+
+      attr_reader :access
+    end
+  end
+end
